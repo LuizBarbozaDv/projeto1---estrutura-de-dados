9ea9b96a9ed9d02b6141fa94c20f5bc703dd074a
5168036 <5168036@liae.edu.uniube.br>
2026-03-26 10:32:50 -0300

    Atualizando classe: 'No'

--- projeto1 - ed/src/No.java
+++ projeto1 - ed/src/No.java
@@ -1,2 +1,34 @@
 public class No {
+
+    private int dado;
+    private No prox;
+
+    // construtor
+
+    public No (int dado) {
+        this.dado = dado;
+        this.prox = null;
+    }
+
+    // getters
+
+    public int getDado() {
+        return dado;
+    }
+
+    public No getProx() {
+        return prox;
+    }
+
+    // setters
+
+    public void setDado (int dado) {
+        this.dado = dado;
+    }
+
+    public void setProx (No prox) {
+        this.prox = prox;
+    }
+
+
 }
